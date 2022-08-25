6be5bef8bfd34a0287cb0def48ac032b4737c4f0
Iqbal Hossain <iqbal.hossain@sslwireless.com>
2022-08-25 14:34:55 +0600

    login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 200);
+        }
+
+        $credentials = request(['email', 'password']);
+        if (Auth::attempt($credentials)) {
+            $response = response()->json([
+                'data' => Auth::user(),
+                'message' => 'success',
+                'code' => 200
+            ], 200);
+        } else {
+            $response = response()->json([
+                'data' => null,
+                'message' => 'failed',
+                'code' => 422
+            ], 200);
+        }
+
+        return $response;
+    }
+
+
+}
